048d3463a2b76da97c6151b7d9561735de881537
liam-murphy14 <liam.murphy137@gmail.com>
2022-07-03 11:48:44 -0400

    update

--- optimize.zsh
+++ optimize.zsh
@@ -3,119 +3,107 @@
 start_date="2005-01-01"
 end_date="2015-01-01"
 start_cash="10000"
-number_opts="2"
-shifts=0
 
 
 help_function () {
-    echo "Usage: optimize.zsh [options] <project name>"
+    echo "Usage: optimize.zsh [options] <project name> [opt 1 name] [opt 1 start] [opt 1 end] [opt 1 step] ..."
     echo
     echo "OPTIONS"
     echo
-    echo "-h : print help function and exit"
+    echo "-h --help : print help function and exit"
     echo
-    echo "-s <start date> : set start date, if not given, defaults to $start_date"
+    echo "-s --start_date <start date> : set start date, if not given, defaults to $start_date"
     echo
-    echo "-e <end date> : set end date, if not given, defaults to $end_date"
+    echo "-e --end_date <end date> : set end date, if not given, defaults to $end_date"
     echo
-    echo "-c <starting cash> : set starting cash, if not given, defaults to $start_cash"
-    echo
-    echo "-o <number of optimization params> : set number of optimization parameters, if not given, defaults to $number_opts"
+    echo "-c --start_cash <starting cash> : set starting cash, if not given, defaults to $start_cash"
 }
 
-while getopts "hs:e:c:o:" option; do
-    case $option in
-    h) # help
+pos_args=()
+
+while [[ $# > 0 ]]; do
+  case $1 in
+    -h|--help)
         help_function
         exit 0
         ;;
-    s) # set start date
-        start_date=$OPTARG
-        shifts=$(($shifts + 2))
-        ;;
-    e) # set end date
-        end_date=$OPTARG
-        shifts=$(($shifts + 2))
-        ;;
-    c) # set start cash
-        start_cash=$OPTARG
-        shifts=$(($shifts + 2))
-        ;;
-    o) # number of optimizations 
-        number_opts=$OPTARG
-        shifts=$(($shifts + 2))
-        ;;
-    \?) # invalid option
-        echo "Error: invalid option, exiting."
-        exit
-        ;;
-    esac
-done
-
-# move args
-while [[ $shifts > 0 ]]; do
-    shift
-    shifts=$(($shifts - 1))
+    -s|--start_date)
+      start_date=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--end_date)
+      end_date=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--start_cash)
+      start_cash=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      pos_args+=( $1 ) # save positional arg
+      shift # past argument
+      ;;
+  esac
 done
 
 # check for valid project
-if [[ ! -d "container/$1" ]]; then
-    echo "bad project name, exiting."
+if [[ ! -d "container/$pos_args[1]" ]]; then
+    echo "bad project name $1, exiting."
     exit 1
-elif [[ ! $1 ]]; then
+elif [[ ! $pos_args[1] ]]; then
     echo "no project name given, exiting."
     exit 1
 fi
-
-if [[ $number_opts > 3 ]]; then
-    echo "too many parameters: max 3, exiting."
-    exit 1
-elif [[ $number_opts < 1 ]]; then
-    echo "must optimize something, exiting."
-    exit 1
-fi
-
-param_array=$2
 
 # activate venv
 # MUST run from QC_Projects directory
 source venv/bin/activate
 cd container
 
-case $number_opts in
-    1)
+proj_name=$pos_args[1]
+pos_args=("${pos_args[@]:1}")
+
+case ${#pos_args[@]} in
+    4)
         echo "BEGINNING OPTIMIZATION"
-        name1=$param_array[1]
-        start1=$param_array[2]
-        end1=$param_array[3]
-        step1=$param_array[4]
+        name1=$pos_args[1]
+        start1=$pos_args[2]
+        end1=$pos_args[3]
+        step1=$pos_args[4]
         for (( i=$start1; i<=$end1; i+=$step1 )); do
             # this is where i would actually do the backtest
             echo
-            python3 ../param_updater.py $1 $start_date $end_date $start_cash $name1 $i
-            lean cloud backtest $1 --push
+            python3 ../param_updater.py $proj_name $start_date $end_date $start_cash $name1 $i
+            lean cloud backtest $proj_name --push
             echo
         done
         deactivate
         echo "FINISHED OPTIMIZATION"
         exit 0
         ;;
-    2)
+    8)
         echo "BEGINNING OPTIMIZATION"
-        name1=$param_array[1]
-        start1=$param_array[2]
-        end1=$param_array[3]
-        step1=$param_array[4]
-        name2=$param_array[5]
-        start2=$param_array[6]
-        end2=$param_array[7]
-        step2=$param_array[8]
+        name1=$pos_args[1]
+        start1=$pos_args[2]
+        end1=$pos_args[3]
+        step1=$pos_args[4]
+        name2=$pos_args[5]
+        start2=$pos_args[6]
+        end2=$pos_args[7]
+        step2=$pos_args[8]
         for (( i=$start1; i<=$end1; i+=$step1 )); do
             for (( j=$start2; j<=$end2; j+=$step2 )); do
                 # this is where i would actually do the backtest
                 echo
-                echo python3 ../param_updater.py $1 $start_date $end_date $start_cash $name1 $i $name2 $j
-                lean cloud backtest $1 --push
+                echo python3 ../param_updater.py $proj_name $start_date $end_date $start_cash $name1 $i $name2 $j
+                lean cloud backtest $proj_name --push
                 echo
             done
         done
@@ -123,27 +111,27 @@ case $number_opts in
         echo "FINISHED OPTIMIZATION"
         exit 0
         ;;
-    3)
+    12)
         echo "BEGINNING OPTIMIZATION"
-        name1=$param_array[1]
-        start1=$param_array[2]
-        end1=$param_array[3]
-        step1=$param_array[4]
-        name2=$param_array[5]
-        start2=$param_array[6]
-        end2=$param_array[7]
-        step2=$param_array[8]
-        name3=$param_array[9]
-        start3=$param_array[10]
-        end3=$param_array[11]
-        step3=$param_array[12]
+        name1=$pos_args[1]
+        start1=$pos_args[2]
+        end1=$pos_args[3]
+        step1=$pos_args[4]
+        name2=$pos_args[5]
+        start2=$pos_args[6]
+        end2=$pos_args[7]
+        step2=$pos_args[8]
+        name3=$pos_args[9]
+        start3=$pos_args[10]
+        end3=$pos_args[11]
+        step3=$pos_args[12]
         for (( i=$start1; i<=$end1; i+=$step1 )); do
             for (( j=$start2; j<=$end2; j+=$step2 )); do
                 for (( k=$start3; k<=$end3; k+=$step3 )); do
                     # this is where i would actually do the backtest
                     echo
-                    echo python3 ../param_updater.py $1 $start_date $end_date $start_cash $name1 $i $name2 $j $name3 $k
-                    lean cloud backtest $1 --push
+                    echo python3 ../param_updater.py $proj_name $start_date $end_date $start_cash $name1 $i $name2 $j $name3 $k
+                    lean cloud backtest $proj_name --push
                     echo
                 done
             done
